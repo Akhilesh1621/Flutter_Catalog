f7dfe788e765bb91250f239a33cf2f983baa7c30
akhilesh <akhileshdevop@gmail.com>
2021-05-20 18:41:12 +0530

    vx

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:e_com/pages/login_page.dart';
 import 'package:e_com/utils/routes.dart';
 import 'package:e_com/utils/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
